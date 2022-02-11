31f9f00eaf009265d4754d201214028672cc3e95
Sivaram Shabari <sivaramsbr1@gmail.com>
2022-02-11 16:18:04 +0530

    :wrench: Fix config file

--- src/config.ts
+++ src/config.ts
@@ -1,15 +1,17 @@
 import { initializeApp } from "firebase/app";
-export const RASA_CHAT_URL = "http://localhost:5005/webhooks/rest/webhook";
-//process.env.RASA_CHAT_URL ||
-//"https://nlu-server-amnjutsbkq-el.a.run.app/webhooks/rest/webhook";
+export const RASA_CHAT_URL =
+	//"http://localhost:5005/webhooks/rest/webhook";
+	process.env.RASA_CHAT_URL ||
+	"https://nlu-server-amnjutsbkq-el.a.run.app/webhooks/rest/webhook";
 
 export const SPEECH_TO_TEXT_URL =
 	process.env.SPEECH_TO_TEXT_URL ||
 	"https://speech-to-text-amnjutsbkq-el.a.run.app/audio";
 
-export const BACKEND_URL = "http://localhost:8080";
-//process.env.BACKEND_URL ||
-//"https://learn-assist-database-amnjutsbkq-el.a.run.app";
+export const BACKEND_URL =
+	//"http://localhost:8080";
+	process.env.BACKEND_URL ||
+	"https://learn-assist-database-amnjutsbkq-el.a.run.app";
 const firebaseConfig = {
 	apiKey: "AIzaSyAXz5iRnCRZ9B38t6xlavStcha4d0bci4s",
 	authDomain: "learnassist-bf264.firebaseapp.com",
